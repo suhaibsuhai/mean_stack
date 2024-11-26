0b02e59404e466862d9b122ea5823f86a82b58b9
suhaibsuhai <suhaibhamza300@gmail.com>
2024-11-26 19:45:20 +0000

    updated backend code

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
 const itemRoutes = require('./routes/item.routes');
-const dbConfig = require('./config/db.config');
 
 const app = express();
 
@@ -13,27 +11,16 @@ app.use(cors());
 app.use(bodyParser.json()); // Parse JSON data from request bodies
 
 // Connect to MongoDB
-mongoose.connect(dbConfig.MONGO_URI)
-  .then(() => console.log('MongoDB connected successfully'))
+mongoose.connect("mongodb://mongodb-mean-stack:g1ScSFXHU0CFPTFC5hlpsS08FUGvCKXI46ggS9H0YFnQEgveNCLL9Go8E9UG8cHzW28IhscpXEGOACDbyBeBSw==@mongodb-mean-stack.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000")
+  .then(() => console.log('MongoDB to Azure connected successfully'))
   .catch((err) => console.error('MongoDB connection failed:', err));
 
 // Set up API routes for items
 app.use('/api/items', itemRoutes);
 
-// Serve static files (frontend) in production
-if (process.env.NODE_ENV === 'production') {
-  // Serve the static files from the dist directory
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist', 'frontend')));
-
-  // Any route that doesn't match the API will return the index.html file
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend', 'index.html'));
-  });
-}
-
 // Define a simple home route
 app.get('/', (req, res) => {
-  res.send('Welcome to the MEAN CRUD API!');
+  res.send('Welcome to the MEAN CRUD API v2!');
 });
 
 // Set the port and start the server
